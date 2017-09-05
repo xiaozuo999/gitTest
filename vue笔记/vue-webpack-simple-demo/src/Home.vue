4aec3fd959470392769d892477c6415bf4d218c5
zhuqiao <chrome0423@gmail.com>
2017-09-05 09:49:44 +0800

    初始化vue项目

---
+++ vue笔记/vue-webpack-simple-demo/src/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h3>我是新闻列表页</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
