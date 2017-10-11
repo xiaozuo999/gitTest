4caba7fbe125cb2a1ee62b9623c964106cff4ce6
zhuqiao <chrome0423@gmail.com>
2017-10-11 16:09:45 +0800

    update

---
+++ projects/elemeAll-vue/src/components/footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="footer-menu df tac">
+    <a href="home.html" class="fx1">
+      <i class="fa fa-shopping-basket" :class="{'c-active':url.indexOf('home')>0 ? true:false}"></i>
+      <span>外卖</span>
+    </a>
+    <a href="search.html" class="fx1">
+      <i class="fa fa-search" :class="{'c-active':url.indexOf('search')>0 ? true:false}"></i>
+      <span>搜索</span>
+    </a>
+    <a href="order.html" class="fx1">
+      <i class="fa fa-reorder " :class="{'c-active':url.indexOf('order')>0 ? true:false}"></i>
+      <span>订单</span>
+    </a>
+    <a href="profile.html" class="fx1">
+      <i class="fa fa-user-o" :class="{'c-active':url.indexOf('profile')>0 ? true:false}"></i>
+      <span>我的</span>
+    </a>
+  </footer>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                url:location.href,
+            }
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ projects/elemeAll-vue/src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <a href="search.html" class="header-left">ele.me</a>
+    <div class="header-center"></div>
+    <a href="index.html" class="header-right fr"><i class="fa fa-user-o" aria-hidden="true"></i></a>
+  </header>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+
+</style>
